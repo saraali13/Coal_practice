a6abb03b6649a9c6b03614c5af57b151313fa1ac
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-03-19 21:58:14 +0500

    Create Q5.asm

---
+++ Basic /Q5.asm
@@ -0,0 +1,38 @@
+INCLUDE Irvine32.inc
+
+.data
+myArray DWORD 10, 20, 30, 40 ,50  ; DWORD array
+
+.code
+main PROC
+    mov eax, [myArray + 4]  ; Direct-offset Operand
+
+    ; Use OFFSET to get the address of myArray
+    mov esi, OFFSET myArray     ; ESI points to the start of myArray
+
+    ; Use indirect addressing 
+    mov eax, [esi]              ; EAX = 10 [esi] get the value pointed by the esi
+    call WriteInt               
+    call Crlf                  
+
+    ; Use indexed addressing 
+    mov eax, myArray[4]         ; EAX = 20
+    call WriteInt               
+    call Crlf                  
+
+    ; Use TYPE, LENGTHOF, and SIZEOF operators
+    mov eax, TYPE myArray       ; EAX = 4 (size of DWORD)
+    call WriteInt              
+    call Crlf                  
+
+    mov eax, LENGTHOF myArray   ; EAX = 5 (number of elements)
+    call WriteInt              
+    call Crlf                  
+
+    mov eax, SIZEOF myArray     ; EAX = 20 (total size in bytes)
+    call WriteInt               
+    call Crlf                  
+
+    exit
+main ENDP
+END main
