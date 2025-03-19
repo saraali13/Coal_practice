b3b0e9603e34a31d5d553a684c85563e750208c2
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-03-19 21:07:57 +0500

    Update Q1.asm

--- Basic /Q1.asm
+++ Basic /Q1.asm
@@ -1,1 +1,16 @@
+INCLUDE Irvine32.inc
 
+.data
+Var1 BYTE 4 ;SBYTE for signed
+Var2 WORD 23
+Var3 DWORD 234
+v QWORD 3456
+.code 
+main proc
+mov al,Var1 ;8 bit data
+mov ax,Var2 ;16 bit data 
+mov eax,Var3 ;32 bit data
+mov rax,v ;64 bit 
+ ;movsx eax Var1 for signed 
+ ;movzx eax,Var1 for transfering small bit data to large bit register
+ ;mov al, BYTE PTR Var2 for transfering llarge bit data to small bit register
