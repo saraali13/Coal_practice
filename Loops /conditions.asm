c4bb7ae726f5b553822b9ce9784edddb454c803b
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-03-20 01:31:04 +0500

    Create conditions.asm

---
+++ Loops /conditions.asm
@@ -0,0 +1,18 @@
+JE ;jump if equal
+JG/JNLE ;jump if greater
+JL/JNGE ;jump if less
+JGE/JNL ;jump if greater and equal
+JLE/JNG ;jump if less and equal
+JNE ;jump if not equal
+  
+;for flags
+JZ ;if zero
+JZN ;if not zero
+JC ;if carry
+JNC ;if not carry
+JO ;if overflow
+JNO ;if not overflow
+JS ;if signed
+JNS ;if not signed
+JP ;if parity
+JNP ;if not parity
