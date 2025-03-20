447a1f05ba6800848ee524e539c167f052d64a1d
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-03-20 08:49:48 +0500

    Create peocedure.asm

---
+++ Loops /peocedure.asm
@@ -0,0 +1,17 @@
+Clrscr ;clear the console
+Crlf ;next line
+WriteBin ;write binary
+WriteChar ;write a single character
+WriteDec ;write decimal
+WriteHex ;write hexa
+WriteInt ;write dec with sign 
+WriteString ;write a complete string
+ReadChar ;read a single character as user input
+ReadDec ;read a dec number as user input
+ReadHex ;read a hexa as user input
+ReadInt ;read an int as user input
+Delay(eax) ;pauses the program
+randomize ;random number generator 
+DumpRegs ;display regiters 
+DumpMem ;display memory
+getDateTime ;current date and time of the system
