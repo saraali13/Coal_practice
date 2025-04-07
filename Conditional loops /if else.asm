06ef543b005da39711b048795a0e4be2d27dcc29
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-04-08 00:07:39 +0500

    Update if else.asm

--- Conditional loops /if else.asm
+++ Conditional loops /if else.asm
@@ -1,8 +1,16 @@
-
+include irvine32.inc
+.data
+op1 BYTE 4
+op2 BYTE 6
+.code
+main proc
 CMP op1, op2         ; Compare op1 with op2
 JNE Else             ; Jump to Else if they are not equal
-MOV X, 1             ; If equal, X = 1
+MOV ax, 1             ; If equal, X = 1
 JMP Endif            ; Jump to Endif
 Else:
-MOV X, 2             ; If not equal, X = 2
+MOV ax, 2             ; If not equal, X = 2
 Endif:
+ exit
+main ENDP
+END main
