b1d61f7d06f79d86caa60f8e45bc7b5256620f5d
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-05-08 01:29:29 +0500

    Create Q2.asm

---
+++ String /Q2.asm
@@ -0,0 +1,17 @@
+.data
+string1 BYTE ‘this is first string’ ,0
+string2 BYTE ‘this is second string’,0
+.code main PROC
+cld ; clear direction flag
+mov esi,OFFSET string1 ; ESI points to source
+mov edi,OFFSET string2 ; EDI points to target
+mov ecx,sizeof string1 ; set counter to 10
+rep movsb ; move bytes
+mov edx,offset string2
+call writestring main ENDP
+END main
+
+;rep -- repeat with ecx (>0)
+;repz and repnz (zero flag and ecx>0)
+;repe and repne 
+
